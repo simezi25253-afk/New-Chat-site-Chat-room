57c8d2c03e4cc308424689ff541641ff7acf2239
simezi25253-afk <simezi25253@gmail.com>
2026-03-24 12:48:21 +0900

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -4,6 +4,7 @@ const MessageSchema = new mongoose.Schema({
   roomId: { type: String, required: true },
   username: { type: String, required: true },
   message: { type: String, required: true },
+  readBy: { type: [String], default: [] }, // 既読ユーザー
   createdAt: { type: Date, default: Date.now }
 });
 
